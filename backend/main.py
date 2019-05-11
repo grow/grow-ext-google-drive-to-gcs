affde87d77d4d60cd00f48a6aee864198eb647ae
Jeremy Weinstein <jeremydw@gmail.com>
2019-05-10 21:46:47 -0700

    Update frontend for media uploads.

--- backend/main.py
+++ backend/main.py
@@ -21,13 +21,16 @@ class MainHandler(webapp2.RequestHandler):
     def post(self):
         folder_id = self.request.POST['folder_id']
         gcs_path_format = self.request.POST['gcs_path_format']
-        tag = 'replicate-{}'.format(folder_id)
-        tasks = uploader.download_resource(resource_id=folder_id, gcs_path_format=gcs_path_format, tag=tag)
+        tag = '{}-{}'.format(os.getenv('REQUEST_ID_HASH', ''), folder_id)
+        parent_tag = uploader.get_parent_tag(folder_id)
+        from google.appengine.ext import deferred
+        deferred.defer(uploader.create_tag, folder_id, gcs_path_format, tag, parent_tag)
+        # tasks = uploader.download_resource(resource_id=folder_id, gcs_path_format=gcs_path_format, tag=tag, parent_tag=parent_tag)
         kwargs = {
             'folder_id': folder_id,
             'gcs_path_format': gcs_path_format,
             'tag': tag,
-            'tasks': tasks,
+#            'tasks': tasks,
 #            'paths': paths,
         }
         self.render_template('index.html', kwargs)
@@ -38,13 +41,27 @@ class MainHandler(webapp2.RequestHandler):
         gcs_path_format = self.request.get('gcs_path_format')
         entities = None
         if tag:
-            query = uploader.GoogleCloudStorageUploadStatus.query()
-            query = query.filter(uploader.GoogleCloudStorageUploadStatus.tag == tag)
+            query = uploader.Asset.query()
+            query = query.filter(uploader.Asset.tag == tag)
+            query = query.order(uploader.Asset.basename)
             entities = query.fetch()
+        parent_entity_ids_to_entities = {}
+        parent_tag = uploader.get_parent_tag(folder_id)
+        if parent_tag:
+            parent_query = uploader.Asset.query()
+            query = query.filter(uploader.Asset.tag == parent_tag)
+            query = query.order(uploader.Asset.basename)
+            parent_entities = query.fetch()
+            for ent in parent_entities:
+                parent_entity_ids_to_entities[ent.drive_id] = ent
+        tag_ent = uploader.get_tag(tag) if tag else None
         kwargs = {
             'folder_id': folder_id,
             'gcs_path_format': gcs_path_format,
+            'parent_entity_ids_to_entities': parent_entity_ids_to_entities,
             'entities': entities,
+            'tag': tag,
+            'tag_ent': tag_ent,
         }
         self.render_template('index.html', kwargs)
 
--- backend/uploader.py
+++ backend/uploader.py
@@ -1,4 +1,6 @@
 from google.appengine.api import app_identity
+from google.appengine.ext import blobstore
+from google.appengine.api import images
 from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 from google.appengine.api import urlfetch_errors
@@ -53,11 +55,24 @@ class UploadRequiredError(Error):
     pass
 
 
-class GoogleCloudStorageUploadStatus(ndb.Model):
-    tag = ndb.StringProperty()
-    bucket_path = ndb.StringProperty()
-    status = ndb.StringProperty()
+class Tag(ndb.Model):
+    num_assets = ndb.IntegerProperty()
     modified = ndb.DateTimeProperty(auto_now=True)
+
+
+class Asset(ndb.Model):
+    basename = ndb.StringProperty()
+    bucket_path = ndb.StringProperty()
+    drive_id = ndb.StringProperty()
+    etag = ndb.StringProperty()
+    folder_id = ndb.StringProperty()
+    mimetype = ndb.StringProperty()
+    modified = ndb.DateTimeProperty(auto_now=True)
+    parent_tag = ndb.StringProperty()
+    serving_url = ndb.StringProperty()
+    status = ndb.StringProperty()
+    tag = ndb.StringProperty()
+    title = ndb.StringProperty()
 
 
 service = None
@@ -113,7 +128,18 @@ def download_folder(resource_id, process_deletes=True):
     return child_resource_responses
 
 
-def download_resource(resource_id, gcs_path_format=None, tag=None):
+def create_tag(resource_id, gcs_path_format, tag, parent_tag):
+    tasks = download_resource(resource_id, gcs_path_format, tag, parent_tag)
+    tag = Tag(key=ndb.Key('Tag', tag))
+    tag.num_assets = len(tasks)
+    tag.put()
+
+
+def get_tag(tag):
+    return ndb.Key('Tag', tag).get()
+
+
+def download_resource(resource_id, gcs_path_format=None, tag=None, parent_tag=None):
     service = get_service()
     resp = service.files().get(fileId=resource_id).execute()
     if 'mimeType' not in resp:
@@ -129,8 +155,8 @@ def download_resource(resource_id, gcs_path_format=None, tag=None):
         logging.info('Skipping file due to incompatible mimetype: {} ({})'.format(title, mime_type))
     else:
         logging.info('Processing file: {} ({})'.format(title, resource_id))
+        return deferred.defer(replicate_asset_to_gcs, resp, gcs_path_format, tag=tag, parent_tag=parent_tag)
         # NOTE: Can use deferred here instead.
-        return deferred.defer(replicate_asset_to_gcs, resp, gcs_path_format, tag=tag)
         return replicate_asset_to_gcs(resp, gcs_path_format=gcs_path_format)
 
 
@@ -147,24 +173,47 @@ def process_folder_response(resp, gcs_path_format, tag=None):
     return uploaded_paths
 
 
-def replicate_asset_to_gcs(resp, gcs_path_format, tag=None):
-    bucket_path = os.path.join(gcs_path_format, resp['title'])  # /foo/bar/baz/file.png
-    status = GoogleCloudStorageUploadStatus(tag=tag, bucket_path=bucket_path, status='started')
-    status.put()
+def get_parent_tag(folder_id):
+    query = Asset.query()
+    query = query.filter(Asset.folder_id == folder_id)
+    query = query.order(-Asset.modified)
+    assets = query.fetch(limit=1)
+    return assets[0].tag if assets else None
+
+
+def replicate_asset_to_gcs(resp, gcs_path_format, tag=None, parent_tag=None, upload_to_cloud_images=True):
+    drive_id = resp['id']
+    etag = resp['etag']
+    mimetype = resp['mimeType']
+    title = resp['title']
+    basename = title
+    # tag is: <request hash>-<folder id>
+    folder_id = tag.split('-')[-1]
+
+    bucket_path = os.path.join(gcs_path_format, title)  # /foo/bar/baz/file.png
+    asset = Asset(tag=tag, bucket_path=bucket_path, status='started',
+            drive_id=drive_id, title=title, basename=basename, mimetype=mimetype,
+            etag=etag, folder_id=folder_id, parent_tag=parent_tag)
+    asset.put()
     path = '/'.join(bucket_path.lstrip('/').split('/')[1:])  # bar/baz
     bucket = bucket_path.lstrip('/').split('/')[0]  # foo
     if True or not appengine_config.DEV_SERVER:
         try:
             stat = gcs.stat(bucket_path)
-            if stat.etag != resp['etag']:
+            if stat.etag != etag:
                 raise UploadRequiredError()
             raise UploadRequiredError()
         except (gcs.NotFoundError, UploadRequiredError):
             # TODO - Verify if asset needs to be replicated.
-            fp = download_asset_in_parts(resp['id'])
-            write_gcs_file(path, bucket, fp, resp['mimeType'])
-            status.status = 'finished'
-            status.put()
+            fp = download_asset_in_parts(drive_id)
+            write_gcs_file(path, bucket, fp, mimetype)
+    if ('png' in mimetype or 'jpeg' in mimetype) and upload_to_cloud_images:
+        gs_path = '/gs/{}'.format(bucket_path.lstrip('/'))
+        blob_key = blobstore.create_gs_key(gs_path)
+        url = images.get_serving_url(blob_key, secure_url=True)
+        asset.serving_url = url
+    asset.status = 'finished'
+    asset.put()
     return bucket_path
 
 
@@ -209,8 +258,22 @@ def write_gcs_file(path, bucket, fp, mimetype):
     storage_service = get_storage_service()
     media = http.MediaIoBaseUpload(
         fp, mimetype=mimetype, chunksize=GCS_UPLOAD_CHUNKSIZE, resumable=True)
+    email = 'grow-prod@appspot.gserviceaccount.com'
+    # allUsers:READER needed for Google Dynamic Image Service uploads.
+    acl = [{
+        'role': 'READER',
+        'entity': 'allUsers',
+    }, {
+        'role': 'OWNER',
+        'entity': 'user-{}'.format(email),
+        'email': email,
+    }]
+    body = {
+        'name': path,
+        'acl': acl,
+    }
     req = storage_service.objects().insert(
-        media_body=media, name=path, bucket=bucket)
+        media_body=media, name=path, bucket=bucket, body=body)
     resp = None
     backoff = BACKOFF
     retries = 0
